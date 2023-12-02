ce721f549f2cdd720b2961a6cf802887a13a0ec6
nazem0_home_pc <mohamednazem09@gmail.com>
2023-12-02 17:17:40 +0200

    Mapping Profiles

---
+++ Infrastructure/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Domain.DTOs.AttachmentDTOs;
+using Domain.DTOs.CityDTOs;
+using Domain.DTOs.GovernorateDTOs;
+using Domain.DTOs.ReportDTOs;
+using Domain.Entities;
+
+namespace Infrastructure
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            //Use ReverseMap to make two way mapping.
+            CreateMap<Attachment, AttachmentViewDTO>();
+            CreateMap<Governorate, GovernorateViewDTO>();
+            CreateMap<City, CityViewDTO>();
+            CreateMap<Report, ReportViewDTO>();
+            CreateMap<ReportAdditionDTO, Report>();
+        }
+    }
+}
