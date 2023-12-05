a0ba771f81e1c98eb657cf383e875f512a90bfa1
nazem0_home_pc <mohamednazem09@gmail.com>
2023-12-06 01:33:10 +0200

    dependencies from infra layer

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Domain.IRepositories;
+using Domain;
+using Infrastructure.Persistence;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection InfrastructureDependencyInjection(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfiles))
+            .AddScoped<IUnitOfWork, UnitOfWork>()
+            .AddScoped<IReportRepository, ReportRepository>();
+            return services;
+        }
+    }
+}
