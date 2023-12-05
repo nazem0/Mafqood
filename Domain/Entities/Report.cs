40f6479f84d4cb58fcf1f7b3b4d685b4be14eb8e
nazem0_home_pc <mohamednazem09@gmail.com>
2023-12-05 22:39:46 +0200

    added facial hair column

--- Domain/Entities/Report.cs
+++ Domain/Entities/Report.cs
@@ -1,17 +1,22 @@
-﻿namespace Domain.Entities
+﻿using Domain.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
 {
     public class Report : BaseModel
     {
         public string? Name { get; set; }
         public bool Gender { get; set; }
-        public int SkinTone { get; set; }
-        public int HairType { get; set; }
-        public int HairColor { get; set; }
-        public int EyeColor { get; set; }
-        public int HeightLevel { get; set; }
-        public int WeightLevel { get; set; }
-        public int? MinAge { get; set; }
-        public int? MaxAge { get; set; }
+        public SkinTone SkinTone { get; set; }
+        public HairType HairType { get; set; }
+        public bool FacialHair { get; set; }
+        public HairColor HairColor { get; set; }
+        public EyeColor EyeColor { get; set; }
+        public HeightLevel HeightLevel { get; set; }
+        public WeightLevel WeightLevel { get; set; }
+        public byte? MinAge { get; set; }
+        public byte? MaxAge { get; set; }
         public string? Street { get; set; }
         public string? District { get; set; }
         public int CityId { get; set; }
@@ -19,9 +24,10 @@
         public required string ContactNumber { get; set; }
         public string? AdditionalInfo { get; set; }
         public bool Valid { get; set; } = false; // false by default
-        public bool Missing { get; set; } = false; // false by default
+        public bool Missing { get; set; } = true; // false by default
         public required string DeletionCode { get; set; } // pass to delete report
         public virtual ICollection<Attachment> Attachments { get; set; } = new List<Attachment>();
         public virtual City City { get; set; }
+
     }
 }
